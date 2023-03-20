4dd7d5ce7b04f45a3adce245d25b096ca30ffe22
the7goonies <54484779+the7goonies@users.noreply.github.com>
2023-03-20 08:33:24 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('https://api.streamelements.com/kappa/v2/activities/59b478950d3fde75addb52b9?after=2023-03-20T06%3A40%3A22.108Z&before=2023-03-20T20%3A31%3A32.108Z&limit=500&mincheer=1&minhost=1&minsub=1&mintip=0&origin=0&types=subscriber', {
+      const response = await fetch('https://api.streamelements.com/kappa/v2/activities/59b478950d3fde75addb52b9?after=2023-03-20T06%3A40%3A22.108Z&before=2023-03-20T20%3A31%3A32.108Z&limit=5000&mincheer=1&minhost=1&minsub=1&mintip=0&origin=0&types=subscriber', {
         headers: {
           Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNTllODhhODlmZDY5MTAxNTM5ZGZkNDFmIiwicm9sZSI6Im93bmVyIiwiY2hhbm5lbCI6IjU5YjQ3ODk1MGQzZmRlNzVhZGRiNTJiOSIsInByb3ZpZGVyIjoidHdpdGNoIiwiYXV0aFRva2VuIjoiVWp1blZ6VWl0VXFEQ2MwRENaaXFoNS1oeVI1MDdTcEc2Y3hhQk00SFRwRm5VTTlsIiwiaWF0IjoxNjc2MTk1OTU0LCJpc3MiOiJTdHJlYW1FbGVtZW50cyJ9.tbk2Ouiy6p9GDBvV2dpZ9iuE9gmwcFfIwslDnkmoET0'
         }
